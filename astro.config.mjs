64cc5f296fdc0465eaaa76168f8b0be1087f0fa0
0xFronto <aurelianpsyche@proton.me>
2025-08-28 02:39:52 +1000

    Configure Astro deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-// astro.config.mjs
 
 export default defineConfig({
-  site: 'https://0xfronto.github.io', // <--  GitHub Pages URL
+  site: 'https://0xfronto.github.io',
+  output: 'static', 
 });
-
-

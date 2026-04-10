5f07bd301c5f51e94d4b4dd21f25158202f7cc1d
0xFronto <aurelianpsyche@proton.me>
2026-04-10 19:42:23 +1000

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  site: 'https://0xN22.github.io',
-  output: 'static', 
+  site: 'https://0xn22.github.io',
+  output: 'static',
 });
-
-
-
 
 
 
